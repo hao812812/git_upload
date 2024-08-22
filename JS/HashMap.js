438e62801836b62ea0f23aaad720ec5403c28209
“hao_00550331” <howard881122@gmail.com>
2024-08-22 16:50:13 +0800

    [摘要] 第三次push

--- JS/HashMap.js
+++ JS/HashMap.js
@@ -1,27 +1,27 @@
 var HashMap = function () {
-    let map = {};
-    return {
-      //新增資料
-      put: function (key, value) {
-        map[key] = value;
-      },
-      //全部key回傳
-      keys: function () {//*object.keys -> 傳出陣列
+  let map = {};
+  return {
+    //新增資料
+    put: function (key, value) {
+      map[key] = value;
+    },
+    //全部key回傳
+    keys: function () {//*object.keys -> 傳出陣列
 
-        return Object.keys(map);
-      },
-      //檢查key值
-      contains: function (key) {//*array.includes -> 傳入陣列，判斷是否有包含某個元素
+      return Object.keys(map);
+    },
+    //檢查key值
+    contains: function (key) {//*array.includes -> 傳入陣列，判斷是否有包含某個元素
 
-        return Object.keys(map).includes(key);
-      },
-      //回傳key的value
-      get: function (key) {
-        return map[key];
-      },
-      //清空map
-      clear: function () {
-        map = {};
-      }
-    };
+      return Object.keys(map).includes(key);
+    },
+    //回傳key的value
+    get: function (key) {
+      return map[key];
+    },
+    //清空map
+    clear: function () {
+      map = {};
+    }
   };
+};
